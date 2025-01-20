fd896456067727d10d5a177c0e8de1af51da4334
ElvisH2024 <elvis.hodzic@iths.se>
2025-01-20 11:48:35 +0100

    Added

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+const mongoose = require("mongoose");
 
 
-app.listen(PORT, () => {
-    console.log("listening to 3000");
+app.get("/", (req, res) => {
+    res.send ("Welcome to my mongoAPI");
+});
+
+
+app.post("/blogpost", (req, res) => {
+    res.send("this is a postrequest");
+});
+
+
+
+mongoose.
+connect("mongodb+srv://elvishodzic888:Bosnia12@clusterapi.einc1.mongodb.net/Products-API?retryWrites=true&w=majority&appName=ClusterAPI")
+.then(() => {
+    console.log("connected to mongo");
+    app.listen(PORT, () => {
+        console.log("listening to 3000");
+    });
 });
