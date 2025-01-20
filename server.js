82cf97bedefbd60f2079089c5a78e88bc30b3a78
ElvisH2024 <elvis.hodzic@iths.se>
2025-01-20 15:08:36 +0100

    added more endpoints

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const PORT = 3000;
 const mongoose = require("mongoose");
-
+const Product = require("./models/productModel");
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send ("Welcome to my mongoAPI");
@@ -13,10 +15,49 @@ app.post("/blogpost", (req, res) => {
     res.send("this is a postrequest");
 });
 
+app.put("/api/product/:id", async(req,res) => {
+     const {id} = req.params;
+     const product = await Product.findByIdAndUpdate(id, req.body);
+     
+     if(!product){
+        return res.status(400).json({message: "Product does not exist"});
+ }
+
+ res.status(200).json(product);
+});
+
+app.get("/api/product/:id", async(req, res) => {
+    try {
+        const {id} = req.params;
+        const product = await Product.findById(req.params.id);
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500);
+    }
+})
+
+app.get("/api/products", async(req, res) =>{
+    try {
+        const products = await Product.find({});
+        res.status(200).json(products)
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
+
+app.post("/api/product", async (req, res) => {
+    try {
+        const product = await Product.create(req.body);
+        res.status(200).json(product);
+    } catch (error) {
+        console.log(error.message);
+        res.status(404);
+    }
+});
 
 
 mongoose.
-connect("mongodb+srv://elvishodzic888:Bosnia12@clusterapi.einc1.mongodb.net/Products-API?retryWrites=true&w=majority&appName=ClusterAPI")
+connect(process.env.MONGO_URI)
 .then(() => {
     console.log("connected to mongo");
     app.listen(PORT, () => {
